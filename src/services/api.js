11e1ca660f836c525b0f7be236381e4f88c569a3
CollinDex <obetscollins@gmail.com>
2024-10-19 01:13:19 +0100

    fix deploy url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://job-board-api-production.up.railway.app/api/v1/';
+const API_BASE_URL = 'https://job-board-api-d5jm.onrender.com/api/v1/';
+//const API_BASE_URL = 'https://job-board-api-production.up.railway.app/api/v1/';
 //const API_BASE_URL = 'http://localhost:8000/api/v1/';
 
 const api = axios.create({
